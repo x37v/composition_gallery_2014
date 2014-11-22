deb5e394d46290f9fdadaf1b93330f2789b78313
Alex Norman <x37v.alex@gmail.com>
2014-11-22 14:04:42 -0800

    kinda progress...

--- control.rb
+++ control.rb
@@ -1,12 +1,22 @@
 require 'rubygems'
 require 'osc-ruby'
+require "unimidi"
+require_relative 'xnormidi'
 
+MIDI_NAME = 'Faderfox DJ3'
+
+xmidi = XNORMIDI.new
+
+@loop_time = 0.001
+@omutex = Mutex.new
 @c = OSC::Client.new('localhost', 9001)
 def osend(addr, val)
-  if val
-    @c.send(OSC::Message.new(addr, val))
-  else
-    @c.send(OSC::Message.new(addr))
+  @omutex.synchronize do
+    if val
+      @c.send(OSC::Message.new(addr, val))
+    else
+      @c.send(OSC::Message.new(addr))
+    end
   end
 end
 
@@ -21,9 +31,45 @@ osend("/formanttime" , 0)
 
 vowels = [ 'ee', 'ih', 'eh', 'ae', 'a', 'oo', 'uh', 'o', 'ah' ].collect { |v| "/vowel_" + v }
 
-osend("/nvca" , 1.0)
+@cc_funcs = {
+  112 => lambda do |val| #left volume
+    osend('/nvca', (val.to_f / 127))
+  end,
+  0 => lambda do |val| #left filter
+    @loop_time = val.to_f / 127
+  end
+}
 
-loop do
-  osend(vowels[rand(vowels.size())], nil)
-  sleep 0.001
+xmidi.register(XNORMIDI::CC) do |arr|
+  num = arr[1].to_i
+  val = arr[2].to_i
+  puts "cc chan: #{XNORMIDI::channel(arr[0])} num: #{num} val: #{val}"
+  f = @cc_funcs[num]
+  f.call(val) if f
 end
+xmidi.register(XNORMIDI::NOTEON) do |arr|
+  puts "note on chan: #{XNORMIDI::channel(arr[0])} num: #{arr[1]} vel: #{arr[2]}"
+end
+xmidi.register(XNORMIDI::NOTEOFF) do |arr|
+  puts "note off chan: #{XNORMIDI::channel(arr[0])} num: #{arr[1]} vel: #{arr[2]}"
+end
+
+unimidi = UniMIDI::Input.select { |m| m.name == MIDI_NAME }.first.open
+
+Thread.new do
+  loop do
+    xmidi.process_unimidi(unimidi)
+  end
+end
+
+t = Time.now
+loop do
+  n = Time.now
+  if @loop_time == 0
+    t = n
+  elsif t + @loop_time <= n
+    osend(vowels[rand(vowels.size())], nil)
+    t = n
+  end
+end
+

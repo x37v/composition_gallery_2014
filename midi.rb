a199cda84647d0ea754be7ef342e1d1044753cd0
Alex Norman <x37v.alex@gmail.com>
2014-11-22 13:20:30 -0800

    midi in another thread

--- midi.rb
+++ midi.rb
@@ -15,8 +15,14 @@ xmidi.register(XNORMIDI::NOTEOFF) do |arr|
   puts "note off chan: #{XNORMIDI::channel(arr[0])} num: #{arr[1]} vel: #{arr[2]}"
 end
 
+
 unimidi = UniMIDI::Input.select { |m| m.name == MIDI_NAME }.first.open
-loop do
-  xmidi.process_unimidi(unimidi)
+midithread = Thread.new do
+  loop do
+    xmidi.process_unimidi(unimidi)
+  end
 end
 
+loop do
+  sleep 1
+end

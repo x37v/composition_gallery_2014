6b127fae047668f1b565bad2c6d3e968c74aa2b8
Alex Norman <x37v.alex@gmail.com>
2014-11-22 13:41:21 -0800

    noisy control

--- control.rb
+++ control.rb
@@ -1,5 +1,29 @@
 require 'rubygems'
 require 'osc-ruby'
 
-client = OSC::Client.new('localhost', 3333)
-client.send(OSC::Message.new( "/greeting" , "hullo!"))
+@c = OSC::Client.new('localhost', 9001)
+def osend(addr, val)
+  if val
+    @c.send(OSC::Message.new(addr, val))
+  else
+    @c.send(OSC::Message.new(addr))
+  end
+end
+
+osend("/bvca" , 0.0)
+osend("/nvca" , 0.0)
+osend("/tvca" , 0.0)
+
+osend("/npan" , 0.5)
+
+osend("/bfreq" , 32)
+osend("/formanttime" , 0)
+
+vowels = [ 'ee', 'ih', 'eh', 'ae', 'a', 'oo', 'uh', 'o', 'ah' ].collect { |v| "/vowel_" + v }
+
+osend("/nvca" , 1.0)
+
+loop do
+  osend(vowels[rand(vowels.size())], nil)
+  sleep 0.001
+end

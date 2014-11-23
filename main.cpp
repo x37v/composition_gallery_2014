1b192a6e81b2294d27b846cc5110e4807e02c61d
Alex Norman <x37v.alex@gmail.com>
2014-11-23 12:42:15 -0800

    reduced formant period

--- main.cpp
+++ main.cpp
@@ -53,11 +53,10 @@ int main(int argc, char * argv[]) {
   osc::send("/npan", 0.5);
 
   clk::time_point next_osc = clk::now();
-
   clk::time_point last_update = clk::now();
 
   osc_server::with_float("/formant_period", [&] (float f) {
-    formant_period = milliseconds_type(1 + static_cast<int>(f * 1000.0));
+    formant_period = milliseconds_type(1 + static_cast<int>(f * 500.0));
   });
 
   osc_server::with_float("/formant_center", [&] (float f) {

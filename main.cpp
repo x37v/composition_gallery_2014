fedd56bc6a9ae7f43598afd8aaeaa4f6a9eee06c
Alex Norman <x37v.alex@gmail.com>
2014-11-23 15:36:42 -0800

    ditched the even division of frequency

--- main.cpp
+++ main.cpp
@@ -80,6 +80,9 @@ int main(int argc, char * argv[]) {
   osc::send("/tvca", 0.0);
   osc::send("/bvca", 0.0);
 
+  osc::send("/thpf", 0.0);
+  osc::send("/tlpf", 127.0);
+
   osc::send("/vca1", 1.0);
 
   osc::send("/t1", 64);
@@ -97,11 +100,13 @@ int main(int argc, char * argv[]) {
 
   std::vector<float> tone_offset = {
     0.0,
-    1.0 / 2.0,
+    //1.0 / 2.0,
     1.0 / 3.0,
     1.0 / 5.0,
     1.0 / 7.0,
     1.0 / 11.0,
+    1.0 / 13.0,
+    1.0 / 17.0,
   };
 
   auto set_freqs = [&]{

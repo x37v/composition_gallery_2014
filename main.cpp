549dc491fb0978c60a43cb91e00244c8c9b45f10
Alex Norman <x37v.alex@gmail.com>
2014-11-24 08:25:13 -0800

    some random LEDS with notes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <lo/lo.h>
 #include <vector>
+#include <array>
 #include <mutex>
 #include <chrono>
 #include <random>
 #include "osc_server.h"
+
+#define NUM_LEDS 6
 
 using std::cout;
 using std::endl;
@@ -20,6 +23,18 @@ enum preformance_mode_t {
   NOTES
 };
 
+//primes
+std::vector<float> tone_offset = {
+  0.0,
+  //1.0 / 2.0,
+  1.0 / 3.0,
+  1.0 / 5.0,
+  1.0 / 7.0,
+  1.0 / 11.0,
+  1.0 / 13.0,
+  1.0 / 17.0,
+};
+
 preformance_mode_t performance_mode = NOTES;
 
 typedef std::chrono::steady_clock clk;
@@ -33,6 +48,7 @@ float volume[3] = {0.0f, 0.0f, 0.1};
 
 clk::duration formant_period = milliseconds_type(1000);
 clk::duration osc_period = milliseconds_type(10);
+clk::duration led_period = milliseconds_type(40);
 clk::duration pan_period = milliseconds_type(80);
 
 uni_real_distro formant_rand;
@@ -68,6 +84,12 @@ namespace osc {
     std::lock_guard<std::mutex> lock(osc_mutex);
     lo_send(osc_address, addr.c_str(), "f", v);
   }
+
+  void send_led(int index, float h, float s, float l) {
+    std::lock_guard<std::mutex> lock(osc_mutex);
+    l = std::min(1.0f, std::max(0.0f, static_cast<float>(std::sin(l * M_PI / 2.0))));
+    lo_send(osc_address, ("/led" + std::to_string(index + 1)).c_str(), "fff", h, s, l);
+  }
 }
 
 float frand() {
@@ -101,6 +123,31 @@ void set_volumes(float master) {
   }
 }
 
+bool was_note = false;
+
+std::vector<std::vector<float>> leds;
+int led_offset = 0;
+
+void draw_leds() {
+  if (was_note) {
+    led_offset = frand() * static_cast<float>(NUM_LEDS);
+    for (int i = 0; i < NUM_LEDS; i++) {
+      int l = (led_offset + i) % NUM_LEDS;
+      if (leds[l][2] == 0.0f) {
+        leds[l][0] = frand();
+        leds[l][1] = 1.0;
+        leds[l][2] = std::max(0.0f, volume[TONE] - frand() * 0.1f);
+        break;
+      }
+    }
+  }
+  for (int i = 0; i < NUM_LEDS; i++) {
+    osc::send_led(i, leds[i][0], leds[i][1], leds[i][2]);
+    leds[i][2] = std::max(0.0f, leds[i][2] - 0.047f);
+  }
+  was_note = false;
+}
+
 void note(int& index, clk::time_point& next_note) {
   next_note = clk::now() + milliseconds_type(static_cast<int>(200.0 * frand()));
   int m = index % 2;
@@ -108,6 +155,7 @@ void note(int& index, clk::time_point& next_note) {
     osc::send("/tvca", volume[TONE]);
     osc::send("/nvca", volume[NOISE]);
     osc::send("/bvca", volume[BASS]);
+    was_note = true;
   } else if (m == 1) {
     osc::send("/tvca", 0.0);
     osc::send("/nvca", 0.0);
@@ -122,7 +170,9 @@ void note(int& index, clk::time_point& next_note) {
 int main(int argc, char * argv[]) {
   osc_server::start(10001);
 
-  osc::setup("", "9001");
+  //osc::setup("", "9001"); //main patch
+  osc::setup("", "1888"); //jason's mockup, with route through
+
   osc::send("/nvca", 0.0);
   osc::send("/tvca", 0.0);
   osc::send("/bvca", 0.0);
@@ -130,10 +180,15 @@ int main(int argc, char * argv[]) {
   osc::send("/thpf", 0.0);
   osc::send("/tlpf", 127.0);
 
-  osc::send("/vca" + std::to_string(map_vca(0)), 1.0);
+  osc::send("/vca" + std::to_string(map_vca(0)), 0.0);
 
   osc::send("/t1", 64);
   osc::send("/bfreq", 92);
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds.push_back({0.0f, 0.0f, 0.0f});
+    osc::send_led(i, 0, 0, 0);
+  }
 
   /*
   std::vector<float> tone_offset = {
@@ -146,17 +201,6 @@ int main(int argc, char * argv[]) {
   };
   */
 
-  std::vector<float> tone_offset = {
-    0.0,
-    //1.0 / 2.0,
-    1.0 / 3.0,
-    1.0 / 5.0,
-    1.0 / 7.0,
-    1.0 / 11.0,
-    1.0 / 13.0,
-    1.0 / 17.0,
-  };
-
   auto set_freqs = [&]{
     for (int i = 1; i < 6; i++) {
       float v = 0.05 * frand() + tone_offset[i] * tone_freq_spread;
@@ -165,7 +209,6 @@ int main(int argc, char * argv[]) {
   };
   set_freqs();
 
-  //osc::send("/nvca", 0.5);
   osc::send("/npan", tone_pan);
 
 
@@ -234,6 +277,7 @@ int main(int argc, char * argv[]) {
   clk::time_point next_pan = clk::now();
   clk::time_point next_note = clk::now();
   clk::time_point last_formant = clk::now();
+  clk::time_point next_led = clk::now();
 
   int note_index = 0;
 
@@ -256,6 +300,11 @@ int main(int argc, char * argv[]) {
     if (next_note < n) {
       if (performance_mode == NOTES)
         note(note_index, next_note);
+    }
+
+    if (next_led < n) {
+      draw_leds();
+      next_led = n + led_period;
     }
 
     if (next_pan < n) {
